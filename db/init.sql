703afcb12167792fbb7a6ae02d6efc576edac7d7
enmebaraggesi <koniec.poczatek@gmail.com>
2024-06-21 15:58:22 +0200

    update Song entity

--- db/init.sql
+++ db/init.sql
@@ -1,107 +1,110 @@
-CREATE TABLE IF NOT EXISTS SONG
+CREATE TABLE song
 (
-    ID     BIGSERIAL PRIMARY KEY,
-    ARTIST VARCHAR(255) NOT NULL,
-    NAME   VARCHAR(255) NOT NULL
+    id           BIGSERIAL PRIMARY KEY,
+    name         VARCHAR(255) NOT NULL,
+    artist       VARCHAR(255) NOT NULL,
+    release_date TIMESTAMP WITHOUT TIME ZONE,
+    duration     BIGINT,
+    language     VARCHAR(255)
 );
 
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Ariana Grande', 'Tik Tok');
+VALUES (NEXTVAL('song_id_seq'), 'Ariana Grande', 'Tik Tok');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Shawn Mendes', 'Bad Guy');
+VALUES (NEXTVAL('song_id_seq'), 'Shawn Mendes', 'Bad Guy');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Taylor Swift', 'Shape of You');
+VALUES (NEXTVAL('song_id_seq'), 'Taylor Swift', 'Shape of You');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Ed Sheeran', 'Love Story');
+VALUES (NEXTVAL('song_id_seq'), 'Ed Sheeran', 'Love Story');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Billie Eilish', 'Thank U, Next');
+VALUES (NEXTVAL('song_id_seq'), 'Billie Eilish', 'Thank U, Next');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Adele', 'Believer');
+VALUES (NEXTVAL('song_id_seq'), 'Adele', 'Believer');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Post Malone', 'Senorita');
+VALUES (NEXTVAL('song_id_seq'), 'Post Malone', 'Senorita');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Drake', 'Havana');
+VALUES (NEXTVAL('song_id_seq'), 'Drake', 'Havana');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Justin Bieber', 'Uptown Funk');
+VALUES (NEXTVAL('song_id_seq'), 'Justin Bieber', 'Uptown Funk');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Dua Lipa', 'All About That Bass');
+VALUES (NEXTVAL('song_id_seq'), 'Dua Lipa', 'All About That Bass');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Bruno Mars', 'Rolling in the Deep');
+VALUES (NEXTVAL('song_id_seq'), 'Bruno Mars', 'Rolling in the Deep');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Rihanna', 'Cant Stop the Feeling');
+VALUES (NEXTVAL('song_id_seq'), 'Rihanna', 'Cant Stop the Feeling');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Imagine Dragons', 'Blank Space');
+VALUES (NEXTVAL('song_id_seq'), 'Imagine Dragons', 'Blank Space');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'The Weeknd', 'Shake It Off');
+VALUES (NEXTVAL('song_id_seq'), 'The Weeknd', 'Shake It Off');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Katy Perry', 'Sorry');
+VALUES (NEXTVAL('song_id_seq'), 'Katy Perry', 'Sorry');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Coldplay', 'Happier');
+VALUES (NEXTVAL('song_id_seq'), 'Coldplay', 'Happier');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Beyonce', 'Chandelier');
+VALUES (NEXTVAL('song_id_seq'), 'Beyonce', 'Chandelier');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Maroon 5', 'Royals');
+VALUES (NEXTVAL('song_id_seq'), 'Maroon 5', 'Royals');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Cardi B', 'Wrecking Ball');
+VALUES (NEXTVAL('song_id_seq'), 'Cardi B', 'Wrecking Ball');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Lizzo', 'Thinking Out Loud');
+VALUES (NEXTVAL('song_id_seq'), 'Lizzo', 'Thinking Out Loud');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Sam Smith', 'Hotline Bling');
+VALUES (NEXTVAL('song_id_seq'), 'Sam Smith', 'Hotline Bling');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Taylor Swift', 'Love Yourself');
+VALUES (NEXTVAL('song_id_seq'), 'Taylor Swift', 'Love Yourself');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Ed Sheeran', 'Watermelon Sugar');
+VALUES (NEXTVAL('song_id_seq'), 'Ed Sheeran', 'Watermelon Sugar');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Post Malone', 'Dance Monkey');
+VALUES (NEXTVAL('song_id_seq'), 'Post Malone', 'Dance Monkey');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Ariana Grande', 'Stressed Out');
+VALUES (NEXTVAL('song_id_seq'), 'Ariana Grande', 'Stressed Out');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Shawn Mendes', 'Someone Like You');
+VALUES (NEXTVAL('song_id_seq'), 'Shawn Mendes', 'Someone Like You');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Billie Eilish', 'Blinding Lights');
+VALUES (NEXTVAL('song_id_seq'), 'Billie Eilish', 'Blinding Lights');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Dua Lipa', 'Cant Feel My Face');
+VALUES (NEXTVAL('song_id_seq'), 'Dua Lipa', 'Cant Feel My Face');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Bruno Mars', 'WAP');
+VALUES (NEXTVAL('song_id_seq'), 'Bruno Mars', 'WAP');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Rihanna', 'Firework');
+VALUES (NEXTVAL('song_id_seq'), 'Rihanna', 'Firework');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Imagine Dragons', 'Just the Way You Are');
+VALUES (NEXTVAL('song_id_seq'), 'Imagine Dragons', 'Just the Way You Are');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Katy Perry', 'Grenade');
+VALUES (NEXTVAL('song_id_seq'), 'Katy Perry', 'Grenade');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Beyonce', 'This Love');
+VALUES (NEXTVAL('song_id_seq'), 'Beyonce', 'This Love');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Maroon 5', 'Poker Face');
+VALUES (NEXTVAL('song_id_seq'), 'Maroon 5', 'Poker Face');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Cardi B', 'Shape of You');
+VALUES (NEXTVAL('song_id_seq'), 'Cardi B', 'Shape of You');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Lizzo', 'Call Me Maybe');
+VALUES (NEXTVAL('song_id_seq'), 'Lizzo', 'Call Me Maybe');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Sam Smith', 'We Found Love');
+VALUES (NEXTVAL('song_id_seq'), 'Sam Smith', 'We Found Love');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Taylor Swift', 'Locked Out of Heaven');
+VALUES (NEXTVAL('song_id_seq'), 'Taylor Swift', 'Locked Out of Heaven');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Ed Sheeran', 'Love Me Like You Do');
+VALUES (NEXTVAL('song_id_seq'), 'Ed Sheeran', 'Love Me Like You Do');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Post Malone', 'Radioactive');
+VALUES (NEXTVAL('song_id_seq'), 'Post Malone', 'Radioactive');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Ariana Grande', 'Titanium');
+VALUES (NEXTVAL('song_id_seq'), 'Ariana Grande', 'Titanium');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Shawn Mendes', 'Stay');
+VALUES (NEXTVAL('song_id_seq'), 'Shawn Mendes', 'Stay');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Billie Eilish', 'Happy');
+VALUES (NEXTVAL('song_id_seq'), 'Billie Eilish', 'Happy');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Dua Lipa', 'Royals');
+VALUES (NEXTVAL('song_id_seq'), 'Dua Lipa', 'Royals');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Bruno Mars', 'Blank Space');
+VALUES (NEXTVAL('song_id_seq'), 'Bruno Mars', 'Blank Space');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Rihanna', 'All of Me');
+VALUES (NEXTVAL('song_id_seq'), 'Rihanna', 'All of Me');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Imagine Dragons', 'Counting Stars');
+VALUES (NEXTVAL('song_id_seq'), 'Imagine Dragons', 'Counting Stars');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Katy Perry', 'Take Me to Church');
+VALUES (NEXTVAL('song_id_seq'), 'Katy Perry', 'Take Me to Church');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Beyonce', 'The Hills');
+VALUES (NEXTVAL('song_id_seq'), 'Beyonce', 'The Hills');
 INSERT INTO song (id, artist, name)
-VALUES (nextval('song_id_seq'), 'Maroon 5', 'Despacito');
+VALUES (NEXTVAL('song_id_seq'), 'Maroon 5', 'Despacito');
--- src/main/java/com/songify/song/model/Song.java
+++ src/main/java/com/songify/song/model/Song.java
@@ -3,23 +3,34 @@ package com.songify.song.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.Instant;
+
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "song")
+@Table
 public class Song {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(unique = true, nullable = false)
     private Long id;
+    
     @Column(nullable = false)
     String name;
+    
     @Column(nullable = false)
     String artist;
+    
+    Instant releaseDate;
+    
+    Long duration;
+    
+    @Enumerated(EnumType.STRING)
+    SongLanguage language;
     
     public Song(String name, String artist) {
         this.name = name;
