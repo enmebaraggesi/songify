de23c9d5ca6bdc002a33deb7ef7c76b7e7e6e0c4
enmebaraggesi <koniec.poczatek@gmail.com>
2024-07-09 18:44:00 +0200

    added MockMVC to integration test

--- src/integration/java/feature/HappyPathIntegrationTest.java
+++ src/integration/java/feature/HappyPathIntegrationTest.java
@@ -2,21 +2,31 @@ package feature;
 
 import com.songify.SongifyApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 @SpringBootTest(classes = SongifyApplication.class)
 @Testcontainers
 @ActiveProfiles("integration")
+@AutoConfigureMockMvc
 class HappyPathIntegrationTest {
     
     @Container
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+    
+    @Autowired
+    public MockMvc mvc;
     
     @DynamicPropertySource
     public static void propertySet(DynamicPropertyRegistry registry) {
@@ -24,7 +34,7 @@ class HappyPathIntegrationTest {
     }
     
     @Test
-    public void f() {
-    
+    public void f() throws Exception {
+        mvc.perform(get("/songs").contentType(MediaType.APPLICATION_JSON));
     }
 }
