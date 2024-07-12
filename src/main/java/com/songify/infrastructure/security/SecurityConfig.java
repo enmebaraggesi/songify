2375806ae32c978db7ac218734c3b67387d4103b
enmebaraggesi <koniec.poczatek@gmail.com>
2024-07-12 17:55:24 +0200

    added BCrypt to encode passwords

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -4,7 +4,7 @@ import com.songify.domain.usercrud.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -17,6 +17,7 @@ class SecurityConfig {
     
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/resources/db_migration/inserts/V20__insert_users.sql
+++ src/main/resources/db_migration/inserts/V20__insert_users.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (email, password, authorities, enabled)
 VALUES
-    ('bartek', '12345', '{ROLE_ADMIN, ROLE_USER}', true),
+    ('bartek', '$2a$10$jytkSF01F5F4qNEXuhED8O5.CTlJQJqhszpffdeRPqLoVgFBlt9TG', '{ROLE_ADMIN, ROLE_USER}', true),
     ('john', '12345', '{ROLE_USER}', true);
