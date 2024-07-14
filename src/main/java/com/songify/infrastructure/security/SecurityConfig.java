e39b992ae6f8607c3c26c5b1e5a82929afc6f9e8
enmebaraggesi <koniec.poczatek@gmail.com>
2024-07-14 11:42:36 +0200

    unlocked viewing saved Songify assets to all

--- src/main/java/com/songify/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/songify/infrastructure/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.songify.infrastructure.security;
 import com.songify.domain.usercrud.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -35,6 +36,10 @@ class SecurityConfig {
                 .requestMatchers("/swagger-ui/**").permitAll() // potem: pozwalam wchodzić na api swaggera bez autoryzacji
                 .requestMatchers("/swagger-resources/**").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/songs/**").permitAll() // potem: pozwalam wyświetlać dane zapisane w bazie danych
+                .requestMatchers(HttpMethod.GET, "/artists/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/albums/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/genres/**").permitAll()
                 .anyRequest().authenticated()); // finalnie: zapytania można robić już tylko za pomocą uprzedniego logowania
         return http.build();
     }
